9299266f0b97cdcb24e09f8fb6a1f824d4af8060
taqfu <2buzesn@gmail.com>
2017-01-03 16:45:50 -0500

    Finished ballot creation view

--- public/js/js.js
+++ public/js/js.js
@@ -2,4 +2,33 @@ $(document.body).ready(function () {
     $(document).on('keyup', '#new-post-title', function(event){
         $("#new-post-title-num-of-chars").html(150-$("#new-post-title").val().length);
     });
+    $(document).on('change', 'select[name=measure]', function(event){
+        var measureVal = $("select[name=measure]").val();
+        measureVal!="" 
+          ? $("input.create-ballot").removeClass('hidden')
+          : $("input.create-ballot").addClass('hidden');
+        measureVal=="ban user" || measureVal=="new mod" || measureVal=='remove mod' 
+          || measureVal=='new rule' || measureVal=='remove rule'
+          ? $("div#text-input").removeClass('hidden')
+          : $("div#text-input").addClass('hidden');
+        switch(measureVal){
+            case "new mod":
+                $("#text-input-caption").html("New Moderator's Username:");
+                break;
+            case "remove mod":
+                $("#text-input-caption").html("Moderator's Username:");
+                break;
+            case "new rule":
+                $("#text-input-caption").html("New Rule:");
+                break;
+            case "remove rule":
+                $("#text-input-caption").html("Rule #:");
+                break;
+            case "ban user":
+                $("#text-input-caption").html("Username:");
+                break;
+                
+        }
+        
+    });
 });
--- resources/views/Ballot/create.blade.php
+++ resources/views/Ballot/create.blade.php
@@ -1,18 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-    <form method="POST" action="{{route('ballot.store')}}">
-        <label>Mob:
-            <select name='mob_id' class=''>
-                @foreach ($subscriptions as $subscription)
-                    <option value='{{$subscription->mob_id}}'>
-                        {{$subscription->mob->name}}
-                    </option>
-                @endforeach
-            </select>
-        </label>
-        <select>
-
-        </select>
+    <div class='col-md-5'></div>
+    <div class='col-md-2'>
+    <form method="POST" action="{{route('ballot.store')}}" class=''>
+        <div class='form-group'>
+            <label>Mob:
+                <select name='mob_id' class='form-control'>
+                    @foreach ($subscriptions as $subscription)
+                        <option value='{{$subscription->mob_id}}'>
+                            {{$subscription->mob->name}}
+                        </option>
+                    @endforeach
+                </select>
+            </label>
+        </div>
+        <div class='form-group'>
+            <label>Ballot Measure:
+                <select name='measure' class='form-control'>
+                    <option></option>
+                    <option value='ban user'>Ban User</option>
+                    <option value='change days for election'>Change Number Of Days For Election</option>
+                    <option value='change num of mods'>Change Number Of Moderators</option>
+                    <option value='new mod'>New Moderator</option>
+                    <option value='new rule'>New Rule</option>
+                    <option value='remove mod'>Remove Moderator</option>
+                    <option value='remove rule'>Remove Rule</option> 
+                    <option value='change anonymity'>Toggle Anonymity</option>
+                </select>
+            </label>
+        </div>
+        <div id='text-input' class='hidden'>
+            <label>
+                <span id='text-input-caption'></span>
+                <input type='text' name='info' class='form-control' />
+            </label>
+        </div>
+        <input type='submit' class='btn btn-default pull-right hidden create-ballot' value='Submit' />
     </form>
+    </div>
+    <div class='col-md-5'></div>
 @endsection
